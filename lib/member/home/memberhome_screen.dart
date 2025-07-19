5bf8404e4be3d8d29b43316cf081d37ef996649a
Mahek Patel <patelmahek788@gmail.com>
2025-07-19 13:51:13 +0530

    19-07-2025

--- lib/member/home/memberhome_screen.dart
+++ lib/member/home/memberhome_screen.dart
@@ -68,7 +68,7 @@ class _MemberHomeScreenState extends State<MemberHomeScreen>
     if (token != null) {
       await FirebaseFirestore.instance
           .collection('members') // or your member collection
-          .doc(memberDoc.id) // replace 
+          .doc(memberDoc.id) // replace with logged in user ID
           .update({'fcmToken': token});
     }
   }
